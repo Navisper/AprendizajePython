88e79ba8f044ccaca67a93436d15b556f2da5e60
Julian rs <alobuenastarde@gmail.com>
2025-05-19 20:39:54 -0500

    navisper dia1

---
+++ Semana1/Navisper/00hola.py
@@ -0,0 +1,23 @@
+#asi se hace un comentario
+#hola mundo en python
+print('jelou guorld')
+
+"""
+Este es un comentario
+de varias lineas
+y no afecta al programa
+"""
+
+#Consultar el tipo de dato de una variable
+# type()
+
+print(type(5)) #int
+print(type(5.0)) #float
+print(type('5')) #str
+print(type(True)) #bool
+print(type([1,2,3])) #list
+print(type((1,2,3))) #tuple
+print(type({1,2,3})) #set
+print(type({'a':1, 'b':2})) #dict
+print(type(None)) #NoneType
+print(type(5+5j)) #complex
---
+++ Semana1/Navisper/01Variables.py
@@ -0,0 +1,29 @@
+#variables
+
+mi_dato = 10
+print(mi_dato)
+
+mi_texto = 'Hola mundo'
+print(mi_texto)
+
+mi_bool = True
+print(mi_bool)
+
+print(mi_bool,mi_texto, mi_dato)
+
+mi_variable_int_a_string = str(mi_dato)
+print(mi_variable_int_a_string)
+print(type(mi_variable_int_a_string))
+
+mi_nombre = input('¿Cuál es tu nombre? ')
+mi_edad = input('¿Cuál es tu edad? ')
+
+print('Hola', mi_nombre, 'tienes', mi_edad, 'años')
+
+#podemos cambiar el tipo de dato de una variable
+mi_variable_int_a_string = str(mi_dato)
+print(mi_variable_int_a_string)
+mi_bool = 'Hola'
+print(type(mi_bool))
+mi_texto = -2j
+print(type(mi_texto))
