ecfc4ce87405a2299219c869f8773be79f638b2e
zeqzaq <cristianarcilamarques123@gmail.com>
2025-05-19 23:02:06 -0500

    Primer ejercicio Print()
    Se realizo los primeros ejercicios de python

---
+++ Semana1/Cristian/00hola.py
@@ -0,0 +1,73 @@
+print('me gusta el nepe')#----------------------------- Primeros ejercicio para aprender Python -----------------------------#
+#----------------------------------------- Ejercicio 00 -----------------------------------------#
+
+#Imprimir en pantalla el mensaje "Hola mundo" y "Hola mundo 2" el mas usado en el mundo de la programacion pra aprender un nuevo lenguaje de programacion
+
+#Solucion:
+print('EJERCICIO 00')#Imprime en pantalla el mensaje "EJERCICIO 00"
+print('Hola mundo')#Print imprime en pantalla lo que quieras ver entre comillas 
+print('Hola mundo 2 \n')#La forma de usarlo es print('lo que quieras ver') en este ejemplo Hola mundo 2
+
+
+
+
+#-------------------------------------------- Fin Ejercicio 00 --------------------------------------------#
+
+#----------------------------------------- Inicio Ejercicio 00.1 -----------------------------------------#
+
+#imprimir en pantalla con la funcion print()
+
+#       1
+#     2 3
+#   4 5 6
+#7 8 9 10
+
+#Solucion:
+print('       1')
+print('     2 3')
+print('   4 5 6')
+print('7 8 9 10 \n')
+
+#------------------------------------------- Fin Ejercicio 00.1 -------------------------------------------#
+
+#----------------------------------------- Inicio Ejercicio 00.2 -----------------------------------------#
+
+#imprimir en pantalla con la funcion print() el siguiente patron:
+
+#DDDD   L
+#D   D  L
+#D   D  L
+#D   D  L
+#D   D  L
+#D   D  L
+#DDD    LLLLL
+
+#Solucion:
+print('DDDD   L')
+print('D   D  L')
+print('D   D  L')
+print('D   D  L')
+print('D   D  L')
+print('D   D  L')
+print('DDD    LLLLL \n')
+
+#------------------------------------------- Fin Ejercicio 00.2 -------------------------------------------#
+
+#----------------------------------------- Inicio Ejercicio 00.3 -----------------------------------------#
+
+#imprimir en pantalla lo siguiente:
+# 1.Today's date.
+# 2. How you are feeling right now.
+# 3. What you want to accomplish by learning to code.
+# 4. A little message to your older, wiser, and programmer self.
+# 5. Your favorite emoji to spice things up!
+
+#Solucion:
+print('1. Today\'s date: 19/05/2025')#Imprime en pantalla la fecha de hoy
+print('2. How you are feeling right now: Excited!')#Imprime en pantalla como te sientes
+print('3. What you want to accomplish by learning to code: Build amazing things!')#Imprime en pantalla lo que quieres lograr
+print('4. A little message to your older, wiser, and programmer self: Keep pushing forward! \n')#Imprime en pantalla un mensaje para ti mismo
+
+#------------------------------------------- Fin Ejercicio 00.3 -------------------------------------------#
+
+#Fin del ejercicio 00 en donde se imprimen diferentes mensajes en pantalla usando la funcion print()
